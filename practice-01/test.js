cb9096029f3107884f3a27213894b4062d86f873
prakash jha <prakashjha7330@gmail.com>
2025-02-05 17:30:03 +0530

    a prog...

--- practice-01/test.js
+++ practice-01/test.js
@@ -1,1 +1,34 @@
-console.log ("prakash")
+
+// function redecorate() {
+//     let room = "Bedroom";
+    
+//     if (true) {
+//       let room = "Kitchen";
+//       var color = "Blue";
+//     }
+    
+//     console.log(room);  // bedroom
+//     console.log(color); // blue
+//   }
+  
+//   redecorate();
+
+
+
+// function disappearingBox() {
+//     if (false) {
+//       var snack = "Cookie";
+//       let drink = "Juice";
+//     }
+    
+//     console.log(snack); // What happens here?
+//     console.log(juice); // What happens here?
+//   }
+  
+//   disappearingBox();
+
+let original = "cookie";  
+let copy = original; // You get a new cookie!  
+
+copy = "cupcake"; // Change the copy  
+console.log(original); // Still "cookie" 🍪  
